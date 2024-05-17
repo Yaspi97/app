93f12b3f55344b3d78a6c80a4ac1761317e77429
yaspi97 <yaspi97@proton.me>
2024-05-17 09:47:20 +0000

    maj

--- Sonarqube-Jenkins-trivy/install.sh
+++ Sonarqube-Jenkins-trivy/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adop>
 sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
@@ -16,7 +16,7 @@ sudo systemctl status jenkins
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
-sudo usermod -aG docker jenkins  
+sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
